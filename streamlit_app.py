b40855fd78feb978bd5d052130680c2362156c16
Soumyadipta Kar <69068577+SoumyadiptaKar@users.noreply.github.com>
2024-11-14 21:23:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,15 @@ st.title('🐧 Machine Learning App')
 
 st.info('This is a machine learning app.')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
+
+  st.write('**X**')
+  X = df.drop('species',axix=1)
+  X
+
+  st.write('**y**')
+  y=df.species
+  y
